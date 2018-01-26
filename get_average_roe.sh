1669638fbf7621800ea55851114aedb92e584014
Jack Kwong <kwong999999@gmail.com>
2018-01-26 13:34:06 +0800

    refactor: abstracted reporting into a function

--- get_average_roe.sh
+++ get_average_roe.sh
@@ -3,10 +3,17 @@ source lib.sh
 url="`getUrlFinancialRatio "$symbol"`"
 echo "URL: \"$url\"" >&2
 html="`getNormalizedHTML "$url"`"
-values="`getFiveYearsFiguresForField 'Return on Equity (%)' "$html"`"
 
-echo "last 5 historical ROE:"
-echo "$values" | paste -s -
+function reportField {
+    local fieldName="$1"
+    local html="$2"
+    local values="`getFiveYearsFiguresForField "$fieldName" "$html" 2>/dev/null`"
 
-echo "5-years arithmetic mean of ROE:"
-arithmeticMean "$values"
+    echo "last 5 historical $fieldName:"
+    echo "$values" | paste -s -
+
+    echo "5-years arithmetic mean of $fieldName:"
+    arithmeticMean "$values" 2>/dev/null
+}
+
+reportField 'Return on Equity (%)' "$html"
