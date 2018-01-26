5a6d3ef4a3e562aa390da8fff1c903d9f737af46
Jack Kwong <kwong999999@gmail.com>
2018-01-26 13:45:41 +0800

    adjusted formatting & included two more fields about debt ratios

--- get_average_roe.sh
+++ get_average_roe.sh
@@ -2,6 +2,9 @@ symbol=$1
 source lib.sh
 url="`getUrlFinancialRatio "$symbol"`"
 echo "URL: \"$url\"" >&2
+echo
 html="`getNormalizedHTML "$url"`"
 
+reportField 'Long Term Debt/Equity (%)' "$html"
+reportField 'Total Debt/Equity (%)' "$html"
 reportField 'Return on Equity (%)' "$html"
